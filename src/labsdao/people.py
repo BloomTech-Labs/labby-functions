cc63bcd81953848d0651485826ea63626c2e86ba
Bernie Durfee <berniedurfee@users.noreply.github.com>
2021-02-03 16:02:11 -0500

    Added get_all_bw_students function

--- src/labsdao/people.py
+++ src/labsdao/people.py
@@ -8,6 +8,26 @@ from airtable import Airtable
 SMT_BASE_ID = "appvMqcwCQosrsHhM"
 
 STUDENTS_SURVEYS_TABLE = "Labs - TBSurveys"
+STUDENTS_TABLE = "STUDENTS"
+
+STUDENTS_TABLE_BW_VIEWS = {"FT": "BW Students (FT)", "PTPT": "BW Students (PTPT)", "PTCT": "BW Students (PTCT)"}
+
+
+def get_all_bw_students(bw_section: str) -> list:
+    """
+    Retrieves records for all students currently in a particular BW section
+
+    Parameters:
+        bw_section (``str``): One of "FT", "PTPT", "PTCT"
+    Returns:
+        records (``list``): List of student records
+    """
+    students_table = Airtable(SMT_BASE_ID, STUDENTS_TABLE, api_key=os.environ["AIRTABLE_API_KEY"])
+
+    return students_table.get_all(
+        view=STUDENTS_TABLE_BW_VIEWS[bw_section],
+        fields=["First Name", "Last Name", "Lambda Email", "Active Track Team"],
+    )
 
 
 def get_all_student_surveys(cohort: str) -> list:
